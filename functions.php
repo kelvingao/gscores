c998ae9cbf8651e827e34c8b4e89e6a4d1e5452c
root <root@dnsledger.cloud.mos>
2018-12-10 01:36:42 -0500

    03_05

--- functions.php
+++ functions.php
@@ -84,6 +84,44 @@ endif;
 add_action( 'after_setup_theme', 'gscores_setup' );
 
 /**
+ * Register custom fonts.
+ */
+function gscores_fonts_url() {
+	$fonts_url = '';
+
+	/*
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'gscores' );
+
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'gscores' );
+
+	$font_families = array();
+
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+	}
+
+	if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+	}
+
+	if (in_array('on', array($source_sans_pro, $pt_serif))) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
@@ -120,6 +158,9 @@ add_action( 'widgets_init', 'gscores_widgets_init' );
  * Enqueue scripts and styles.
  */
 function gscores_scripts() {
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style( 'gscores-fonts', gscores_fonts_url() );
+	
 	wp_enqueue_style( 'gscores-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'gscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
